6decd31bb88ac8e999c7e9efa3c172bbd6b0dd86
Yuk Chuen Rick Wang <rickwangg@gmail.com>
2020-01-14 10:01:32 -0500

    Initial Commit

---
+++ WeekOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeekOne
+//
+//  Created by Yuk Chuen Rick Wang on 1/14/20.
+//  Copyright © 2020 Yuk Chuen Rick Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
